e823a12f2c8bf5f58d2b79f85b144ec1687a45ef
bobygoon <bobygoon7@gmail.com>
2023-01-07 01:17:05 +0400

    add task 64

---
+++ FP/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine($"Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.");
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
