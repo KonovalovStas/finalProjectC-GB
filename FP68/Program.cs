8fb016693bc41077bbe1a4710b4bfa0b83423580
bobygoon <bobygoon7@gmail.com>
2023-01-07 01:28:03 +0400

    add task68

---
+++ FP68/Program.cs
@@ -0,0 +1,21 @@
+﻿int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
